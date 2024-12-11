9ae5e502826950e4cc6dd2794d3f63d7cc236c75
valeryio <tommagoma23@gmail.com>
2024-12-11 14:08:48 +0100

    Update the task 0

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ import redisClient from './utils/redis';
 // console.log("On main : ", redisClient.isAlive());
 
 (async () => {
-    console.log(redisClient.isAlive());
-    console.log(await redisClient.get('myKey'));
-    await redisClient.set('myKey', 12, 5);
-    console.log(await redisClient.get('myKey'));
+  console.log(redisClient.isAlive());
+  console.log(await redisClient.get('myKey'));
+  await redisClient.set('myKey', 12, 5);
+  console.log(await redisClient.get('myKey'));
 
-    setTimeout(async () => {
-        console.log(await redisClient.get('myKey'));
-    }, 1000*10)
+  setTimeout(async () => {
+    console.log(await redisClient.get('myKey'));
+  }, 1000 * 10);
 })();
-
